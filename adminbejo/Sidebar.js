dcc4c28ed4c6863daaa7f2b8b0a0a666d78ef206
Walter Bejo Jr. <walterbejo123@gmail.com>
2024-07-17 21:41:48 +0800

    Ok na ang Text naay delay

--- adminbejo/Sidebar.js
+++ adminbejo/Sidebar.js
@@ -1,10 +1,15 @@
 const hamBurger = document.querySelector(".toggle-btn");
 const sidebar = document.querySelector("#sidebar");
-const sidebarLinks = document.querySelectorAll("#sidebar .sidebar-link span");
+const sidebarLinks = document.querySelectorAll("#sidebar .sidebar-link .span-word");
 
 hamBurger.addEventListener("click", function () {
     sidebar.classList.toggle("expand");
-    sidebarLinks.forEach(span => {
-        span.classList.toggle("expand");
-    });
+    
+    if (sidebar.classList.contains("expand")) {
+        setTimeout(() => {
+            sidebar.classList.add("show-text");
+        }, 100); 
+    } else {
+        sidebar.classList.remove("show-text");
+    }
 });
