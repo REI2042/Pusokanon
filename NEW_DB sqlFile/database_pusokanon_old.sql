20c6c7af9cbbff710f23777b55b617ee1e070522
REII <164935035+REI2042@users.noreply.github.com>
2024-07-19 04:18:28 +0800

    changes in DB

--- NEW_DB sqlFile/database_pusokanon_old.sql
+++ NEW_DB sqlFile/database_pusokanon_old.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Jul 18, 2024 at 10:16 PM
+-- Host: 127.0.0.1
+-- Generation Time: Jul 17, 2024 at 04:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -94,6 +94,7 @@ CREATE TABLE `complaints_tbl` (
   `incident_place` varchar(50) NOT NULL,
   `case_type` varchar(50) NOT NULL,
   `narrative` text DEFAULT NULL,
+  `evidence` varchar(200) DEFAULT NULL,
   `staff_id` int(11) NOT NULL,
   `hearing_date` date DEFAULT NULL,
   `hearing_time` time DEFAULT NULL,
@@ -105,15 +106,20 @@ CREATE TABLE `complaints_tbl` (
 -- Dumping data for table `complaints_tbl`
 --
 
-INSERT INTO `complaints_tbl` (`complaint_id`, `res_id`, `respondent_fname`, `respondent_mname`, `respondent_lname`, `respondent_suffix`, `respondent_gender`, `respondent_age`, `incident_date`, `incident_time`, `date_filed`, `incident_place`, `case_type`, `narrative`, `staff_id`, `hearing_date`, `hearing_time`, `status`, `remarks`) VALUES
-(1, 25, 'Nino Rey', 'Yonson', 'Cabunilas', ' ', 'Male', 22, '2024-06-01', '18:30:00', '2024-07-08 06:33:52', 'Matumbo', 'Physical Abuse', 'choke me to death', 2, NULL, NULL, 'Approved', NULL),
-(2, 25, 'Dwight', '', 'Callahan', 'Jr', 'Male', 26, '2024-06-12', '20:00:00', '2024-07-08 06:34:59', 'Cemento', 'Trespassing', 'Ni sulod bisan wala gi invite', 2, NULL, NULL, 'Approved', NULL),
-(3, 28, 'Demi', 'The', 'Great', ' ', 'Female', 26, '2024-03-21', '10:30:00', '2024-07-08 06:48:38', 'Ibabao', 'Theft', 'gikawat amoa sud-an', 2, NULL, NULL, 'Pending', NULL),
-(4, 28, 'Rejie', '', 'Cotoner', ' ', 'Male', 32, '2024-05-25', '11:00:00', '2024-07-08 06:50:10', 'Sta. Maria', 'Damaging Properties', 'this bijj broke my ballpen', 2, NULL, NULL, 'Approved', NULL),
-(5, 28, 'Nino Rey', '', 'Cabunilas', ' ', 'Male', 27, '2024-03-15', '16:30:00', '2024-07-08 06:51:07', 'Mustang', 'Bullying', '', 2, '2024-07-24', '17:36:00', 'Approved', NULL),
-(6, 28, 'Wensly', '', 'Sacay', ' ', 'Male', 23, '2024-05-13', '13:20:00', '2024-07-08 14:41:17', 'Lawis', 'Threat', 'shiwewojdoi', 2, NULL, NULL, 'Declined', 'dwdewsf'),
-(7, 28, 'Luhan', '', 'Doggy', 'III', 'Male', 56, '2024-05-15', '11:30:00', '2024-07-10 10:04:19', 'New Lipata', 'Bullying', 'nang gukod og manok', 2, NULL, NULL, 'Declined', 'dwdwd'),
-(8, 28, 'Reii', '', 'Siko', ' ', 'Male', 25, '2024-07-01', '14:30:00', '2024-07-10 10:08:09', 'Arca', 'Theft', 'gikawat amo iring', 2, '2024-07-24', '18:34:00', 'Approved', NULL);
+INSERT INTO `complaints_tbl` (`complaint_id`, `res_id`, `respondent_fname`, `respondent_mname`, `respondent_lname`, `respondent_suffix`, `respondent_gender`, `respondent_age`, `incident_date`, `incident_time`, `date_filed`, `incident_place`, `case_type`, `narrative`, `evidence`, `staff_id`, `hearing_date`, `hearing_time`, `status`, `remarks`) VALUES
+(1, 28, 'Nino Rey', '', 'Cabunilas', ' ', 'Male', 23, '2024-07-01', '10:30:00', '2024-07-11 07:50:07', 'Arca', 'Bullying', 'tryyy', NULL, 2, '2024-08-01', '08:00:00', 'Accepted', '--'),
+(2, 28, 'Nino Rey', '', 'Cabunilas', ' ', 'Male', 23, '2024-07-01', '10:30:00', '2024-07-11 07:50:47', 'Arca', 'Bullying', 'tryyy', NULL, 2, NULL, NULL, 'Declined', 'can be solved by talking to each other'),
+(3, 28, 'Nino Rey', '', 'Cabunilas', ' ', 'Male', 23, '2024-07-01', '10:00:00', '2024-07-11 07:51:17', 'Arca', 'Bullying', 'tryy', NULL, 2, NULL, NULL, 'Declined', 'ambot. gisapot nakooooooooooooooo'),
+(4, 28, 'Demi', '', 'Great', ' ', 'Male', 23, '2024-07-01', '10:30:00', '2024-07-11 08:02:54', 'Arca', 'Damaging Properties', 'try', 'uploads/668f91aeb8d97.jpg', 2, NULL, NULL, 'Pending', '--'),
+(5, 28, 'Dwight', '', 'Callahan', ' ', 'Male', 54, '2023-12-03', '10:30:00', '2024-07-11 08:04:40', 'Arca', 'Bullying', 'tryy', NULL, 2, NULL, NULL, 'Declined', 'testing 102'),
+(6, 28, 'Dwight', '', 'Callahan', ' ', 'Male', 54, '2023-12-03', '10:30:00', '2024-07-11 08:10:12', 'Arca', 'Bullying', 'tryy', 'uploads/668f93648f193.jpg', 2, '2024-07-31', '14:22:00', 'Accepted', '--'),
+(7, 28, 'waltq', '', 'bejo', ' ', 'Male', 32, '2024-02-05', '11:12:00', '2024-07-11 08:15:48', 'Arca', 'Bullying', 'heiwoi', 'complaints_evidence/668f94b48154c.jpg', 2, '2024-07-30', '09:30:00', 'Accepted', '--'),
+(8, 28, 'Nino Rey', '', 'Cabunilas', ' ', 'Male', 26, '2024-06-30', '11:30:00', '2024-07-11 14:33:43', 'Arca', 'Bullying', 'hahayss', 'complaints_evidence/668fed47e5605.jpg', 2, '2024-07-30', '10:17:00', 'Accepted', '--'),
+(12, 28, 'cawfwcawfw', '', 'cef', ' ', 'Male', 78, '2024-06-30', '11:30:00', '2024-07-12 01:57:56', 'Arca', 'Physical Abuse', 'cawde', 'complaints_evidence/66908da4ec08b.jpg', 2, '2024-07-23', '12:30:00', 'Accepted', '--'),
+(13, 28, 'ndkwewo', '', 'masmdow', ' ', 'Male', 23, '2024-06-30', '10:30:00', '2024-07-12 02:32:08', 'Arca', 'Trespassing', 'okp', 'complaints_evidence/669095a80da60.jpg', 2, '2024-08-08', '09:30:00', 'Accepted', '--'),
+(14, 28, 'Nino Rey', '', 'Cabunilas', ' ', 'Male', 23, '2024-06-03', '15:30:00', '2024-07-15 05:26:30', 'Arca', 'Trespassing', 'bkjopi', NULL, 2, '2024-08-09', '09:30:00', 'Declined', 'testing 103'),
+(15, 28, 'Demi', '', 'Cotoner', ' ', 'Male', 25, '2024-07-01', '10:30:00', '2024-07-15 05:28:35', 'Arca', 'Bullying', 'mkotasdfghjk', 'complaints_evidence/6694b3839701d.jpg', 2, '2024-07-30', '09:30:00', 'Declined', 'di daw tinood'),
+(16, 28, 'Rejie', '', 'Callahan', ' ', 'Male', 32, '2024-07-01', '23:15:00', '2024-07-16 13:13:04', 'Mustang', 'Libel', 'dwightyyyy mylabss', 'complaints_evidence/669671e05e59a.png', 2, '2024-08-01', '21:53:00', 'Accepted', 'testing for transfer');
 
 -- --------------------------------------------------------
 
@@ -225,8 +231,17 @@ CREATE TABLE `request_doc` (
 --
 
 INSERT INTO `request_doc` (`doc_ID`, `res_id`, `docType_id`, `purpose_id`, `purpose_name`, `stat`, `date_req`, `date_processed`, `remarks`, `request_id`, `qrCode_image`) VALUES
-(1, 29, 1, 5, 'damnnn', 'pending', '2024-07-17 18:09:27', '2024-07-17 18:09:28', 'Not released', '!>W$PYst#s}', '1721239768.png'),
-(2, 29, 2, 5, 'For scholarship', 'pending', '2024-07-17 18:12:25', '2024-07-17 18:12:26', 'Not released', '|50{md?ciQ[8b', '1721239946.png');
+(1, 21, 1, 1, 'Employment', 'Pending', '2024-06-26 12:31:51', '2024-07-04 17:05:02', 'Not released', 'O4nx3jm-dA$b=', '1719405111.png'),
+(2, 21, 1, 4, 'Senior Citizen Assistance', 'pending', '2024-06-26 12:32:49', '2024-06-26 12:32:50', 'Not released', 'OYA;F7Ny', '1719405169.png'),
+(3, 21, 1, 4, 'Senior Citizen Assistance', 'pending', '2024-06-26 13:21:44', '2024-06-26 13:21:44', 'Not released', '2ztQ-Mq?.d', '1719408104.png'),
+(4, 13, 1, 4, 'Senior Citizen Assistance', 'pending', '2024-06-27 07:32:31', '2024-06-27 07:32:31', 'Not released', '<+5pzI}4G4qo;', '1719473551.png'),
+(5, 21, 1, 1, 'Employment', 'pending', '2024-06-30 14:32:09', '2024-06-30 14:35:00', 'Not released', 'Ht^XHi3Qn<y', '1719758100.png'),
+(6, 21, 1, 1, 'Employment', 'pending', '2024-07-01 09:00:01', '2024-07-01 09:00:02', 'Not released', 'M;SL*:Yqwj', '1719824402.png'),
+(7, 21, 1, 4, 'Senior Citizen Assistance', 'pending', '2024-07-01 09:09:35', '2024-07-01 09:10:14', 'Released', 'gS!bOhinB', '1719824976.png'),
+(8, 25, 1, 1, 'Employment', 'pending', '2024-07-03 13:44:03', '2024-07-03 13:44:04', 'Not released', 'c@IQ*;>Gyn7Gt', '1720014244.png'),
+(9, 28, 1, 5, 'food allowance', 'pending', '2024-07-08 14:39:26', '2024-07-08 14:39:27', 'Not released', 'T|dc91,XI]', '1720449567.png'),
+(10, 28, 4, 1, 'Employment', 'pending', '2024-07-10 12:51:51', '2024-07-10 12:51:52', 'Not released', 'MI=,1I8!dy', '1720615912.png'),
+(11, 28, 4, 3, 'Person With Disability Assistance', 'pending', '2024-07-10 13:01:55', '2024-07-10 13:01:56', 'Not released', 'n(.IQ:T_w):jd', '1720616516.png');
 
 -- --------------------------------------------------------
 
@@ -268,11 +283,10 @@ INSERT INTO `resident_users` (`res_ID`, `res_fname`, `res_lname`, `res_midname`,
 (18, 'rei', 'cabunilas', 'yonson', 'Jr', 'Female', '2006-11-03', 'Married', 'filipino', 'sewage', '092374823243', 'rei@gmail.com', 'sewage', '', '2402', NULL, 'Registered', 2, NULL, NULL),
 (21, 'Maria Irenea', 'Bebanco', 'Asne', ' ', 'Female', '2003-01-05', 'Single', 'Spanish', 'Cebu City', '09954702461', 'renmarie153@gmail.com', 'Sewage', '4', 'indayamm153', NULL, 'Registered', 2, NULL, NULL),
 (24, 'ninis', 'cabus', 'Yalall', 'Jr', 'Male', '2003-02-23', 'Married', 'Filipino', 'Pusok, sewage', '09682027910', 'myname@gmail.com', 'Sewage', '', '2402', NULL, 'Registered', 2, NULL, NULL),
-(25, 'Walter', 'Bejo', 'Ologuinsan', 'Jr', 'Male', '2002-09-28', 'Single', 'Filipino', 'Cebu', '09329464', 'PJ6J915x8o9TOAacnxoyYeBgMb+kOu5rhxPo4s+u8J0=', 'San Roque', '', '$2y$10$5Di9MKY337fGDf3iT4i5fOUBfLuHQxlHotnHfNw0Yeh6/1ILJMQx2', 'Walter.png', 'Not-registered', 2, '493298', '2024-07-18 21:32:46'),
+(25, 'Walter', 'Bejo', 'Ologuinsan', 'Jr', 'Male', '2002-09-28', 'Single', 'Filipino', 'Cebu', '09329464', 'PJ6J915x8o9TOAacnxoyYeBgMb+kOu5rhxPo4s+u8J0=', 'San Roque', '', '$2y$10$5Di9MKY337fGDf3iT4i5fOUBfLuHQxlHotnHfNw0Yeh6/1ILJMQx2', 'Walter.png', 'Not-registered', 2, NULL, NULL),
 (26, 'Test', 'Test', 'Test', ' ', 'Female', '1997-01-01', 'Single', 'Filipino', 'Cebu', '09329465', '0N2y6dgHJtA+EGEpfGDHyQ==', 'Seaside', '', '$2y$10$E3z70iGSNNUry..guP4rwem3xw578vhK1r7gKdTLF54M7gX6G8chy', NULL, 'Registered', 2, NULL, NULL),
 (27, 'Test2', 'Test2', 'Test2', ' ', 'Female', '2007-01-15', 'Single', 'Filipino', 'Cebu', '12346579810', 'BLMOeJFhqAS/+uo5FHGcrQ==', 'Arca', '', '$2y$10$tJU/gJGvD05EZWUJiiLMUOMlal5Pr3QKyv4VJ30UAYi670DugZC/q', NULL, 'Not-registered', 2, NULL, NULL),
-(28, 'Renee', 'Descartez', '', ' ', 'Female', '2003-10-15', 'Single', 'Filipino', 'Cebu City', '09433930847', 'Ym4cQe4NxD4QWMQz7zFwQBvTR++2kIzfhEAVy/6AK8U=', 'Sewage', '', '$2y$10$xHiGNL3QgFtAc0UkrmYkg.uqJSOpgSNo.wbtEA2M.Tn23FPbT14H6', NULL, 'Registered', 2, NULL, NULL),
-(29, 'Suki', 'Edwards', 'Chantale Buckley', 'III', 'Male', '1991-03-17', 'Married', 'filipino', 'A occaecat aut eos e', '09873452342', 'RttvRYF8taWyJoweINRR7bQg0nhSGo7KfmGgEY5s5hA=', 'Ibabao', '', '$2y$10$2TP4r7zelxwFo4nacwLGCu5yMQh/SQojFUgpRsKLHzrKSqVUHsjnm', NULL, 'Registered', 2, '202669', '2024-07-18 21:34:15');
+(28, 'Renee', 'Descartez', '', ' ', 'Female', '2003-10-15', 'Single', 'Filipino', 'Cebu City', '09433930847', 'Ym4cQe4NxD4QWMQz7zFwQBvTR++2kIzfhEAVy/6AK8U=', 'Sewage', '', '$2y$10$xHiGNL3QgFtAc0UkrmYkg.uqJSOpgSNo.wbtEA2M.Tn23FPbT14H6', NULL, 'Registered', 2, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -354,7 +368,7 @@ ALTER TABLE `barangay_staff`
 -- AUTO_INCREMENT for table `complaints_tbl`
 --
 ALTER TABLE `complaints_tbl`
-  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `docs_purpose`
@@ -372,19 +386,19 @@ ALTER TABLE `doc_type`
 -- AUTO_INCREMENT for table `registration_tbl`
 --
 ALTER TABLE `registration_tbl`
-  MODIFY `res_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `res_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `request_doc`
 --
 ALTER TABLE `request_doc`
-  MODIFY `doc_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `doc_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `resident_users`
 --
 ALTER TABLE `resident_users`
-  MODIFY `res_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `res_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Constraints for dumped tables
