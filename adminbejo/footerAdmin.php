16d4cab6c52b899723e7ad92ba64af7d56b19229
REII <164935035+REI2042@users.noreply.github.com>
2024-08-15 03:13:12 +0800

    removed unused

--- adminbejo/footerAdmin.php
+++ adminbejo/footerAdmin.php
@@ -4,7 +4,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>  
-    <script src="https://smtpjs.com/v3/smtp.js"></script>
+
 
     <script src="Sidebar.js"></script>
 </body>
